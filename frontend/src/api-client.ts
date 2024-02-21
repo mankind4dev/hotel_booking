399c77ba8564abc6deb1b045398e6fd4c89d54d1
mankind4dev <ogunmakin120@gmail.com>
2024-02-21 13:05:33 +0100

    Added my hotels view successfully

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,5 +1,6 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
+import { HotelType} from "../../backend/src/shared/types"
 
 //to start the frontend and backend seperately
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
@@ -57,21 +58,32 @@ export const signOut = async () => {
     method: "POST",
   });
 
-  if(!response.ok){
-    throw new Error("Error during sign out")
+  if (!response.ok) {
+    throw new Error("Error during sign out");
   }
 };
 
 export const addMyHotel = async (hotelFormData: FormData) => {
-  const response = await fetch(`${API_BASE_URL}/api/my-hotels`,{
+  const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
     method: "POST",
     credentials: "include",
     body: hotelFormData,
-  })
-  console.log(response)
-  if(!response.ok){
-    throw new Error("Failed to add hotel")
+  });
+  console.log(response);
+  if (!response.ok) {
+    throw new Error("Failed to add hotel");
   }
 
   return response.json();
-}
+};
+
+export const fetchMyHotels = async (): Promise<HotelType[]> => {
+  const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
+    credentials: "include",
+  });
+  if (!response.ok) {
+    throw new Error("Error fetching hotels");
+  }
+
+  return response.json()
+};
